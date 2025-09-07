307d2c7ebadf53af2179994ecdf75edd7210cbb2
c4vxl <manage@c4vxl.de>
2025-09-07 17:48:54 +0200

    fixed output

--- src/pipeline/base.py
+++ src/pipeline/base.py
@@ -1,5 +1,5 @@
 from PIL import Image
 
 class Pipeline():    
-    def __call__(self, image: Image.Image, **kwargs) -> Image.Image:
+    def __call__(self, image: Image.Image, **kwargs) -> list[Image.Image]:
         return self._generate(image, **kwargs) # type: ignore
