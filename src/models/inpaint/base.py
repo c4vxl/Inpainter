2a9db25cd1c54ffea09c0d3ae94d68eae65267fa
c4vxl <manage@c4vxl.de>
2025-09-08 18:37:11 +0200

    pad mask as well

--- src/models/inpaint/base.py
+++ src/models/inpaint/base.py
@@ -14,5 +14,6 @@ class Inpainter():
     def __call__(self, prompt: str, image: Image.Image, mask: Image.Image, resolution = 2048, **kwargs) -> list[Image.Image]:
         original_size = image.size
         image = pad_to_size(image, resolution)
+        mask = pad_to_size(mask, resolution)
         out = self._generate(prompt, image, mask, **kwargs)
         return [ remove_padding(img, original_size) for img in out ]
